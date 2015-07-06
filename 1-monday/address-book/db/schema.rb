953045504299b187be8b8375cac48a134612d66b
uesteibar <uesteibar@gmail.com>
2015-07-06 20:18:42 +0200

    add column favorite to contacts

--- 1-monday/address-book/db/schema.rb
+++ 1-monday/address-book/db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150706165808) do
+ActiveRecord::Schema.define(version: 20150706181749) do
 
   create_table "contacts", force: :cascade do |t|
     t.datetime "created_at", null: false
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20150706165808) do
     t.string   "address"
     t.string   "phone"
     t.string   "email"
+    t.boolean  "favorite"
   end
 
 end

7932b270587ad68aa210e622072872d253640c77
uesteibar <uesteibar@gmail.com>
2015-07-06 18:28:20 +0200

    use helper for links

--- 1-monday/timetracking/config/routes.rb
+++ 1-monday/timetracking/config/routes.rb
@@ -9,8 +9,9 @@ Rails.application.routes.draw do
   #   get 'products/:id' => 'catalog#view'
   get '/' => 'sites#home'
 
-  get '/projects' => 'projects#index'
-  get '/projects/:id' => 'projects#show'
+  # get '/projects' => 'projects#index'
+  # get '/projects/:id' => 'projects#show'
+  resources :projects, only: [:index, :show]
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

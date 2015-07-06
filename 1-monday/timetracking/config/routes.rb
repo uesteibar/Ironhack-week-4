e4c6ae4a9b299302dc7722188292e2ca142adae9
uesteibar <uesteibar@gmail.com>
2015-07-06 15:42:36 +0200

    create projects controller

--- 1-monday/timetracking/config/routes.rb
+++ 1-monday/timetracking/config/routes.rb
@@ -7,6 +7,8 @@ Rails.application.routes.draw do
 
   # Example of regular route:
   #   get 'products/:id' => 'catalog#view'
+  get '/' => 'site#home'
+  get '/projects' => 'projects#index'
 
   # Example of named route that can be invoked with purchase_url(id: product.id)
   #   get 'products/:id/purchase' => 'catalog#purchase', as: :purchase

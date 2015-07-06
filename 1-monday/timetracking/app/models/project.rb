b3d6456901bfe7e3bc74fa5722243dc25b0eb66c
uesteibar <uesteibar@gmail.com>
2015-07-06 15:23:02 +0200

    playing around with active record

--- 1-monday/timetracking/app/models/project.rb
+++ 1-monday/timetracking/app/models/project.rb
@@ -1,6 +1,13 @@
 
 class Project < ActiveRecord::Base
+  validates_presence_of :name, uniqueness: true
+
   def self.iron_find(date = Date.current)
     where("created_at > ?", date).order(:name).limit(10)
   end
+
+  def self.clean_old(date = 1.week.ago)
+    where("created_at < ?", date).destroy_all
+  end
+
 end
--- 1-monday/timetracking/db/seeds.rb
+++ 1-monday/timetracking/db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+puts "--- Deleting all Projects ---"
+Project.destroy_all
+
+puts "--- Creating 25 Projects ---"
+25.times do |num|
+  Project.create! name: '#{num} project', description: "#{num} project description"
+end

807f315c017b412f8d573d9e85e8922d89b6dccd
uesteibar <uesteibar@gmail.com>
2015-07-06 18:52:49 +0200

    contact routes, model and controller

---
+++ 1-monday/address-book/app/controllers/contact_controller.rb
@@ -0,0 +1,21 @@
+class ContactController < ApplicationController
+  def index
+    @contacts = Contact.all.order(name: :asc)
+    render :index
+  end
+
+  def show
+    @contact = Contact.find(params[:id])
+    render :show
+  end
+
+  def create
+    
+    render :show
+  end
+
+  def destroy
+    Contact.find(params[:id]).destroy
+    render :index
+  end
+end
--- 1-monday/address-book/config/routes.rb
+++ 1-monday/address-book/config/routes.rb
@@ -1,4 +1,6 @@
 Rails.application.routes.draw do
+
+  resources :contacts, only: [:index, :show, :create, :destroy]
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
 

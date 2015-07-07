793c8f81a2721a5fceb239424269d382129f67d0
uesteibar <uesteibar@gmail.com>
2015-07-07 15:22:56 +0200

    create form to add new urls

--- 2-tuesday/url-shortener/app/controllers/urls_controller.rb
+++ 2-tuesday/url-shortener/app/controllers/urls_controller.rb
@@ -9,10 +9,28 @@ class UrlsController < ApplicationController
       render_404(params)
   end
 
+  def new
+    @url = Url.new
+  end
+
+  def create
+    url = Url.new(shortcut: StringShortcutGenerator.new(6).generate, url: url_params[:url])
+    if url.valid?
+      url.save
+      redirect_to url_path(url), notice: "Url was successfully created."
+    else
+      redirect_to action: "new"
+    end
+  end
+
   private
 
   def render_404(params)
     Rails.logger.warn("Tried to access #{params} which did not exist")
     render 'layouts/404'
+  end
+
+  def url_params
+    params.require(:url).permit(:url)
   end
 end

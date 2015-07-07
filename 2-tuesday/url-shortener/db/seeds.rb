9e2971c60b93940ffe02ca727f73946ff7c41ebd
uesteibar <uesteibar@gmail.com>
2015-07-07 14:46:47 +0200

    create seed urls

--- 2-tuesday/url-shortener/db/seeds.rb
+++ 2-tuesday/url-shortener/db/seeds.rb
@@ -1,7 +1,10 @@
-# This file should contain all the record creation needed to seed the database with its default values.
-# The data can then be loaded with the rake db:seed (or created alongside the db with db:setup).
-#
-# Examples:
-#
-#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
-#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+urls = [
+  "http://github.com",
+  "http://medium.com",
+  "http://google.com"
+]
+
+urls.each_with_index do |url, i|
+  Url.create(shortcut: i, url: url)
+end

6dd485ad2eec071215d3e84ad5fd26669b8d0e43
uesteibar <uesteibar@gmail.com>
2015-07-07 16:13:14 +0200

    complete and validate urls

--- 2-tuesday/url-shortener/app/models/url.rb
+++ 2-tuesday/url-shortener/app/models/url.rb
@@ -1,3 +1,14 @@
 class Url < ActiveRecord::Base
   validates_presence_of :shortcut, :url, unique: true
+  validates_format_of :url, :with => URI.regexp(['http'])
+
+  before_validation :complete_url
+
+  private
+
+  def complete_url
+    unless url.starts_with?("http://") || url.starts_with?("https://")
+      url.insert(0, "http://")
+    end
+  end
 end

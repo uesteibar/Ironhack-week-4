0c359bbfb0a0839e8fd2425daefe4288103b79d4
uesteibar <uesteibar@gmail.com>
2015-07-08 13:08:04 +0200

    entry creation

--- 3-wednesday/timetracking/app/controllers/entries_controller.rb
+++ 3-wednesday/timetracking/app/controllers/entries_controller.rb
@@ -3,4 +3,21 @@ class EntriesController < ApplicationController
     @project = Project.find(params[:project_id])
     @entries = @project.month_entries
   end
+
+  def new
+    @project = Project.find(params[:project_id])
+    @entry = @project.entries.new
+  end
+
+  def create
+    @project = Project.find(params[:project_id])
+    @project.entries.create(entry_params)
+    redirect_to action: "index", project_id: @project.id
+  end
+
+  private
+
+  def entry_params
+    params.require(:entry).permit(:date, :hours, :minutes)
+  end
 end
--- 3-wednesday/timetracking/config/routes.rb
+++ 3-wednesday/timetracking/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   get '/' => 'sites#home'
 
   resources :projects, only: [:index, :show] do
-    resources :entries, only: [:index]
+    resources :entries, only: [:index, :new, :create]
   end
 
 end

5a06ca83239e079a16114e885d7fdb7e66c72af5
uesteibar <uesteibar@gmail.com>
2015-07-08 17:09:48 +0200

    create view to create products

--- 3-wednesday/auctions/app/controllers/products_controller.rb
+++ 3-wednesday/auctions/app/controllers/products_controller.rb
@@ -3,4 +3,26 @@ class ProductsController < ApplicationController
     @user = User.find(params[:user_id])
     @products = Product.where(user_id: params[:user_id])
   end
+
+  def new
+    @user = User.find(params[:user_id])
+    @product = @user.products.new
+  end
+
+  def create
+    @user = User.find(params[:user_id])
+    @product = @user.products.new(product_params)
+    if @product.valid?
+      @product.save
+      redirect_to action: "index", user_id: @user.id
+    else
+      render :new
+    end
+  end
+
+  private
+
+  def product_params
+    params.require(:product).permit(:title, :desctiption, :deadline)
+  end
 end
--- 3-wednesday/auctions/config/routes.rb
+++ 3-wednesday/auctions/config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get '/' => "sites#home"
 
   resources :users, only: [:index, :show, :new, :create] do
-    resources :products, only: [:index]
+    resources :products, only: [:index, :new, :create]
   end
 
 end

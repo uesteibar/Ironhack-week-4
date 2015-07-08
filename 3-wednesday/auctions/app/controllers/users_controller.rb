ccf00538569d700cfb5940af9095fe5877f775df
uesteibar <uesteibar@gmail.com>
2015-07-08 17:01:20 +0200

    create users

--- 3-wednesday/auctions/app/controllers/users_controller.rb
+++ 3-wednesday/auctions/app/controllers/users_controller.rb
@@ -6,4 +6,22 @@ class UsersController < ApplicationController
   def show
     @user = User.find(params[:id])
   end
+
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.valid?
+      @user.save
+      redirect_to action: "show", id: @user.id
+    else
+      render :new
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:name, :email)
+  end
 end
--- 3-wednesday/auctions/config/routes.rb
+++ 3-wednesday/auctions/config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
 
   get '/' => "sites#home"
 
-  resources :users, only: [:index, :show] do
+  resources :users, only: [:index, :show, :new, :create] do
     resources :products, only: [:index]
   end
 

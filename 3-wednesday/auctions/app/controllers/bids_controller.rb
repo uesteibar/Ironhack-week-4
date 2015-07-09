a8947a29dbb676cb35fecb42a4eacd27b3cc9290
uesteibar <uesteibar@gmail.com>
2015-07-09 11:06:05 +0200

    restrict bids and deletions to logged in users depending on ownership

--- 3-wednesday/auctions/app/controllers/bids_controller.rb
+++ 3-wednesday/auctions/app/controllers/bids_controller.rb
@@ -1,6 +1,6 @@
 class BidsController < ApplicationController
   def create
-    user = User.find_by(email: user_params[:email])
+    user = User.find(session[:user_id])
     if user.id.present?
       user.bids.create(product_id: product_params[:product_id], amount: bid_params[:amount])
     end
@@ -15,9 +15,5 @@ class BidsController < ApplicationController
 
   def product_params
     params.permit(:product_id)
-  end
-
-  def user_params
-    params.require(:user).permit(:email)
   end
 end

ad3312a1ae5cc85057c8ff13cc50f9cf6cb785a7
uesteibar <uesteibar@gmail.com>
2015-07-10 17:00:59 +0200

    create and validate city model

--- 5-friday/concert-network/db/schema.rb
+++ 5-friday/concert-network/db/schema.rb
@@ -11,7 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150710132421) do
+ActiveRecord::Schema.define(version: 20150710145419) do
+
+  create_table "cities", force: :cascade do |t|
+    t.string   "name"
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+  end
 
   create_table "comments", force: :cascade do |t|
     t.string   "username"
